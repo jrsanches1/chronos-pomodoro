185ff06a8bc2258b7cf2acb01b023619869cb61f
sanchesjoseruy@gmail.com <sanchesjoseruy@gmail.com>
2025-11-24 20:20:31 -0300

    screenshots added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./styles/theme.css";
+import "./styles/global.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Heading } from "./components/Heading";
+import { TimerIcon } from "lucide-react";
 
+export function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <Heading>
+        Ola mundo! 1
+        <button>
+          <TimerIcon />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sapiente
+        dolores qui debitis dolorum ipsum, architecto temporibus, commodi ullam
+        alias accusantium delectus reiciendis. Reprehenderit, accusamus delectus
+        praesentium quam obcaecati dolores!
       </p>
     </>
-  )
+  );
 }
-
-export default App
