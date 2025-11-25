52e28c4c82970fe44b3f4e606f018715c1893d91
sanchesjoseruy@gmail.com <sanchesjoseruy@gmail.com>
2025-11-25 19:22:18 -0300

    started add logos

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,19 @@ import "./styles/theme.css";
 import "./styles/global.css";
 
 import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
 
 export function App() {
   return (
     <>
-      <Heading>
-        Ola mundo! 1
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto sapiente
-        dolores qui debitis dolorum ipsum, architecto temporibus, commodi ullam
-        alias accusantium delectus reiciendis. Reprehenderit, accusamus delectus
-        praesentium quam obcaecati dolores!
-      </p>
+      <Container>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
