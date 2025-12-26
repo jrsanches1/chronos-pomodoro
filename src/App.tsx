f317d99113d30e3ecf4e9116d8c418e90b8c695a
sanchesjoseruy@gmail.com <sanchesjoseruy@gmail.com>
2025-12-26 16:11:08 -0300

    taskModels

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import { Home } from './pages/Home';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { useState } from 'react';
 
 export function App() {
+  const [state, setState] = useState();
+
   return <Home />;
 }
