4dffe2d30fd7359f035c1a705ac9c535c12ee834
sanchesjoseruy@gmail.com <sanchesjoseruy@gmail.com>
2025-12-26 17:42:44 -0300

    props changed

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,36 @@ import { Home } from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
 import { useState } from 'react';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+/* export type TaskStateModel = {
+  tasks: TaskModel[];
+  secondsRemaining: number;
+  formattedSecondsRemaining: string;
+  activeTask: TaskModel | null;
+  currentCycle: number;
+  config: {
+    worktime: number;
+    shortBreakTime: number;
+    longBreakTime: number;
+  };
+}; */
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  const [state, setState] = useState();
+  const [state, setState] = useState(initialState);
 
-  return <Home />;
+  return <Home state={state} setState={setState} />;
 }
